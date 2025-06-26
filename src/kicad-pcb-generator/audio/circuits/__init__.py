99a48ec86874d52a6967bd3dcfc08affe241aa18
codazm <59068340+codazm@users.noreply.github.com>
2025-06-26 17:31:50 -0500

    Create __init__.py

---
+++ src/kicad-pcb-generator/audio/circuits/__init__.py
@@ -0,0 +1,19 @@
+"""Audio circuit templates and utilities."""
+
+from .templates import (
+    AudioCircuitTemplate,
+    PreamplifierTemplate,
+    PowerAmplifierTemplate,
+    EffectsPedalTemplate,
+    AudioInterfaceTemplate,
+    MixingConsoleTemplate
+)
+
+__all__ = [
+    "AudioCircuitTemplate",
+    "PreamplifierTemplate",
+    "PowerAmplifierTemplate",
+    "EffectsPedalTemplate",
+    "AudioInterfaceTemplate",
+    "MixingConsoleTemplate"
+] 
