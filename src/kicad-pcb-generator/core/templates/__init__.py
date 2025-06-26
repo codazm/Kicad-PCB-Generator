9d267fd981d0a91e8d5ba45af62dd6467ebf1e65
codazm <59068340+codazm@users.noreply.github.com>
2025-06-26 18:02:05 -0500

    Create __init__.py

---
+++ src/kicad-pcb-generator/core/templates/__init__.py
@@ -0,0 +1,15 @@
+"""Template system for KiCad Audio Designer."""
+
+from .base import (
+    TemplateBase,
+    LayerStack,
+    ZoneSettings,
+    DesignVariant
+)
+
+__all__ = [
+    "TemplateBase",
+    "LayerStack",
+    "ZoneSettings",
+    "DesignVariant"
+] 
