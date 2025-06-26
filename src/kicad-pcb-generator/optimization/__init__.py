80c8c578d924967ee06705825d8c9c8bfeb14e60
codazm <59068340+codazm@users.noreply.github.com>
2025-06-26 17:34:08 -0500

    Create __init__.py

---
+++ src/kicad-pcb-generator/optimization/__init__.py
@@ -0,0 +1,11 @@
+"""
+Audio optimization module.
+"""
+
+from .advanced_circuit_optimizer import AdvancedCircuitOptimizer, OptimizationType, OptimizationResult
+
+__all__ = [
+    "AdvancedCircuitOptimizer",
+    "OptimizationType",
+    "OptimizationResult"
+] 
