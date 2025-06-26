138822b04cd1ea179b10a0b6297215df4845a5e2
codazm <59068340+codazm@users.noreply.github.com>
2025-06-26 18:10:43 -0500

    Create __init__.py

---
+++ src/kicad-pcb-generator/utils/config/__init__.py
@@ -0,0 +1,11 @@
+"""
+Configuration utilities for KiCad PCB Generator.
+"""
+
+from .settings import Settings, SettingsItem, ValidationSeverity
+
+__all__ = [
+    'Settings',
+    'SettingsItem', 
+    'ValidationSeverity'
+] 
