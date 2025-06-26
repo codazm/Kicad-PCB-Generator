06fd22ef7673db48a601c00152f5348802154261
codazm <59068340+codazm@users.noreply.github.com>
2025-06-26 17:35:37 -0500

    Create __init__.py

---
+++ src/kicad-pcb-generator/simulation/__init__.py
@@ -0,0 +1,11 @@
+"""
+Audio simulation module.
+"""
+
+from .circuit_simulator import CircuitSimulator, SimulationType, SimulationResult
+
+__all__ = [
+    "CircuitSimulator",
+    "SimulationType", 
+    "SimulationResult"
+] 
